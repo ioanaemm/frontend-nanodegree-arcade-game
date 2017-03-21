96eb3f6b7569a8ac8cd8531a5c9b3e027f06e5c3
ioanamitelea <miteleaioana@gmail.com>
2017-03-21 17:39:30 +0000

    added collision between player and enemy

--- js/app.js
+++ js/app.js
@@ -3,6 +3,8 @@ var boardWidth = 5;
 var boardHeight = 6;
 var tileWidth = 101;
 var tileHeight = 83;
+var playerInitialX = 202;
+var playerInitialY = 415;
 var gameWidth = tileWidth * boardWidth;
 var gameHeight = tileHeight * boardHeight;
 
@@ -14,6 +16,7 @@ var Enemy = function(x, y, speed) {
     // a helper we've provided to easily load images
     this.x = x;
     this.y = y;
+    this.initialX = x;
     this.speed = speed;
     this.sprite = 'images/enemy-bug.png';
 };
@@ -26,8 +29,17 @@ Enemy.prototype.update = function(dt) {
     // all computers.
     this.x += dt * this.speed;
     if(this.x > gameWidth) {
-      this.x = 0;
+      this.x = this.initialX;
     }
+
+    if(this.x < player.x + tileWidth && this.x + tileWidth > player.x &&
+       this.y < player.y + tileHeight && this.y + tileHeight > player.y)
+    {
+      player.x = playerInitialX;
+      player.y = playerInitialY;
+    }
+
+
 };
 
 // Draw the enemy on the screen, required method for game
@@ -55,20 +67,64 @@ Player.prototype.render = function () {
 
 Player.prototype.handleInput = function (keyPress) {
 
+  switch(keyPress) {
+    case 'left':
+      move(-tileWidth, 0);
+      break;
+    case 'up':
+      move(0, -tileHeight);
+      break;
+    case 'right':
+      move(tileWidth, 0);
+      break;
+    case 'down':
+      move(0, tileHeight);
+      break;
+  }
+
+  function move(deltaX, deltaY) {
+    if(canMove(deltaX, deltaY)) {
+      player.x += deltaX;
+      player.y += deltaY;
+    }
+  }
+  function canMove(deltaX, deltaY) {
+    if(player.x + deltaX < 0 || player.x + deltaX > gameWidth - tileWidth) return;
+    if(player.y + deltaY < 0 || player.y + deltaY > gameHeight - tileHeight) return;
+    return true;
+  }
 };
 
+function resetPlayer() {
+  player.x = playerInitialX
+  player.y = playerInitialY;
+
+}
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
 
-
+var enemiesInitialPos = [
+  {
+    x: -tileWidth,
+    y: tileHeight * 3 - 25,
+    speed: 60
+  },
+  {
+    x: -tileWidth,
+    y: tileHeight * 3 - 25,
+    speed: 100
+  }
+];
 var allEnemies = [];
-var player = new Player((tileWidth * 2), (tileHeight * 5 - 33));
-var enemy = new Enemy(0, (tileHeight * 3 - 30), 60);
-console.log(enemy);
-
-allEnemies.push(enemy);
+var player = new Player(playerInitialX, playerInitialY);
+console.log(player);
+enemiesInitialPos.forEach(function(enemy){
+  var enemy = new Enemy(enemy.x, enemy.y, enemy.speed);
+  allEnemies.push(enemy);
+  console.log(enemy);
+});
 
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
@@ -80,6 +136,5 @@ document.addEventListener('keyup', function(e) {
         40: 'down'
     };
 
-    player.handleInput(allowedKeys[e.keyCode]);
-    console.log(allowedKeys[e.keyCode]);
+    if(allowedKeys[e.keyCode]) player.handleInput(allowedKeys[e.keyCode]);
 });
