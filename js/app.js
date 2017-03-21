f640b5a3ecfa17c997f20a4bb732a5e5be7f3eeb
ioanamitelea <miteleaioana@gmail.com>
2017-03-21 19:44:39 +0000

    added reset function for player when it reaches water

--- js/app.js
+++ js/app.js
@@ -3,10 +3,11 @@ var boardWidth = 5;
 var boardHeight = 6;
 var tileWidth = 101;
 var tileHeight = 83;
-var playerInitialX = 202;
-var playerInitialY = 415;
+var tileImageHeight = 171;
 var gameWidth = tileWidth * boardWidth;
 var gameHeight = tileHeight * boardHeight;
+
+
 
 var Enemy = function(x, y, speed) {
     // Variables applied to each of our instances go here,
@@ -27,20 +28,29 @@ Enemy.prototype.update = function(dt) {
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
-    this.x += dt * this.speed;
-    if(this.x > gameWidth) {
-      this.x = this.initialX;
-    }
 
-    if(this.x < player.x + tileWidth && this.x + tileWidth > player.x &&
-       this.y < player.y + tileHeight && this.y + tileHeight > player.y)
-    {
-      player.x = playerInitialX;
-      player.y = playerInitialY;
-    }
-
-
+    this.applySpeed(dt);
+    this.checkCollisionWithPlayer();
 };
+
+Enemy.prototype.applySpeed = function (dt) {
+  this.x += dt * this.speed;
+  if(this.x > gameWidth) {
+    this.x = this.initialX;
+  }
+}
+
+Enemy.prototype.checkCollisionWithPlayer = function() {
+  var bugRow = Math.ceil(this.y / tileHeight);
+  var playerRow = player.y / tileHeight;
+  if(this.x < player.x + tileWidth && this.x + tileWidth > player.x &&
+    bugRow == playerRow
+  ) {
+    player.reset();
+  }
+}
+
+
 
 // Draw the enemy on the screen, required method for game
 Enemy.prototype.render = function() {
@@ -54,6 +64,8 @@ Enemy.prototype.render = function() {
 var Player = function (x, y) {
   this.x = x;
   this.y = y;
+  this.initialX = x;
+  this.initialY = y;
   this.sprite = 'images/char-boy.png';
 };
 
@@ -62,43 +74,42 @@ Player.prototype.update = function () {
 };
 
 Player.prototype.render = function () {
-  ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+  ctx.drawImage(Resources.get(this.sprite), this.x, this.y - 30);
 };
 
 Player.prototype.handleInput = function (keyPress) {
-
   switch(keyPress) {
     case 'left':
-      move(-tileWidth, 0);
+      this.move(-tileWidth, 0);
       break;
     case 'up':
-      move(0, -tileHeight);
+      this.move(0, -tileHeight);
       break;
     case 'right':
-      move(tileWidth, 0);
+      this.move(tileWidth, 0);
       break;
     case 'down':
-      move(0, tileHeight);
+      this.move(0, tileHeight);
       break;
-  }
-
-  function move(deltaX, deltaY) {
-    if(canMove(deltaX, deltaY)) {
-      player.x += deltaX;
-      player.y += deltaY;
-    }
-  }
-  function canMove(deltaX, deltaY) {
-    if(player.x + deltaX < 0 || player.x + deltaX > gameWidth - tileWidth) return;
-    if(player.y + deltaY < 0 || player.y + deltaY > gameHeight - tileHeight) return;
-    return true;
   }
 };
 
-function resetPlayer() {
-  player.x = playerInitialX
-  player.y = playerInitialY;
+Player.prototype.move = function (deltaX, deltaY) {
+  if(this.canMove(deltaX, deltaY)) {
+    this.x += deltaX;
+    this.y += deltaY;
+  }
+  if(this.y == 0) this.reset();
+}
+Player.prototype.canMove = function (deltaX, deltaY) {
+  if(this.x + deltaX < 0 || this.x + deltaX > gameWidth - tileWidth) return;
+  if(this.y + deltaY < 0 || this.y + deltaY > gameHeight - tileHeight) return;
+  return true;
+}
 
+Player.prototype.reset = function() {
+  this.x = this.initialX;
+  this.y = this.initialY;
 }
 
 // Now instantiate your objects.
@@ -108,7 +119,7 @@ function resetPlayer() {
 var enemiesInitialPos = [
   {
     x: -tileWidth,
-    y: tileHeight * 3 - 25,
+    y: tileHeight * 2 - 25,
     speed: 60
   },
   {
@@ -118,12 +129,11 @@ var enemiesInitialPos = [
   }
 ];
 var allEnemies = [];
-var player = new Player(playerInitialX, playerInitialY);
-console.log(player);
+var player = new Player(2 * tileWidth, 4 * tileHeight);
+
 enemiesInitialPos.forEach(function(enemy){
   var enemy = new Enemy(enemy.x, enemy.y, enemy.speed);
   allEnemies.push(enemy);
-  console.log(enemy);
 });
 
 // This listens for key presses and sends the keys to your
